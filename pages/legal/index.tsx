9c57878cc0757abfadead8b7094f4788536bb8eb
Michael Naumann <michael.naumann@medicalvalues.de>
2021-07-26 23:02:50 +0200

    Create basic pages settings, projects, and legal

---
+++ pages/legal/index.tsx
@@ -0,0 +1,5 @@
+export default function Legal() {
+    return (
+        <div>Legal site loaded successfully!</div>
+    )
+}
---
+++ pages/projects/index.tsx
@@ -0,0 +1,5 @@
+export default function Projects() {
+    return (
+        <div>Projects site loaded successfully!</div>
+    )
+}
