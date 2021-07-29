c930fb2f874a68f72ea3829dab0a07e7ba644066
Michael Naumann <michael.naumann@medicalvalues.de>
2021-07-29 23:03:05 +0200

    Make MenuButton use svg icon

--- src/components/menuButton/menuButton.tsx
+++ src/components/menuButton/menuButton.tsx
@@ -1,15 +1,25 @@
 import styles from './menuButton.module.css'
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {ThemeContext} from "../../../pages/home";
+import menuIconBlack from '../../assets/img/menu_black_48dp.svg'
+import menuIconWhite from '../../assets/img/menu_white_48dp.svg'
+import Image from 'next/image'
+import {primary} from "../../providers/themeProvider";
 
 export default function MenuButton() {
 
     const theme = useContext(ThemeContext)
+    const [bgColor, setBgColor] = useState<string>(theme.primary)
 
     return (
-            <button
-                className={styles.button}
-                style={{backgroundColor: theme.primary}}>
-            </button>
+        <button
+            className={styles.button}
+            style={{backgroundColor: bgColor}}
+            onMouseEnter={() => setBgColor(theme.secondary)}
+            onMouseLeave={() => setBgColor(theme.primary)}>
+            {theme.primary === primary.light ?
+                <Image src={menuIconBlack} alt={'menu button'}/> :
+                <Image src={menuIconWhite} alt={'menu button'}/>}
+        </button>
     )
 }
