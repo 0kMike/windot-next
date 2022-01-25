b3bbe6b7e064159eb948b5e9fe8d9b6d73b62951
Michael Naumann <michael.naumann@medicalvalues.de>
2022-01-25 07:29:35 +0100

    Add background to settings page

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import styles from './settings.module.css';
 
 export default function Settings() {
-  return <div></div>;
+  return <main className={styles.container}>
+
+  </main>;
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-row h-10 w-full items-center justify-between z-20">
+    <footer className="flex flex-row h-10 w-full items-center justify-between z-20 text-grey-900 dark:text-grey-200 transition duration-500">
       <section></section>
       <section>this will be the footer</section>
       <section></section>
